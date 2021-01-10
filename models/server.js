c1aa4127ba0f856d8a6bc6b0c00099f3e10c16ff
unknown <cclaveria@siliconmedia.com>
2021-01-10 17:28:16 +0100

    CORS

--- models/server.js
+++ models/server.js
@@ -22,8 +22,12 @@ class Server {
         this.server = http.createServer(this.app);
 
         // configuración de sockets
-        this.io = socketio(this.server,
-       
+        this.io = socketio(this.server,{
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+                }
+            }
         );
 
     }
